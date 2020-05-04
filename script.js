b75d74c61a3dd678995621b1bb03dab506e67a4e
Shawn Beaton <beat0154@algonquinlive.com>
2020-05-04 18:13:09 -0400

    upd

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var user = document.getElementById("user");
+var ball = document.getElementById("ball");
+var balldiv = document.getElementById("balldiv");
+var px = "px";
 var mousedownID = -1; 
 function mousedownLeft(event) {
   if(mousedownID==-1)mousedownID = setInterval(whilemousedownLeft, 2);
@@ -17,12 +21,11 @@ function whilemousedownLeft() {
         user.style.left = newnewleft;
     }
 }
-//document.getElementById("left").addEventListener("mousedown", mousedownLeft);
-//document.getElementById("left").addEventListener("mouseup", mouseupLeft);
-document.getElementById("left").addEventListener("onpointerdown", mousedownLeft);
-document.getElementById("left").addEventListener("onpointerup", mouseupLeft);
+document.getElementById("left").addEventListener("mousedown", mousedownLeft);
+document.getElementById("left").addEventListener("mouseup", mouseupLeft);
+document.getElementById("left").addEventListener("ontouchstart", mousedownLeft);
+document.getElementById("left").addEventListener("ontouchend", mouseupLeft);
 
-var mousedownID = -1; 
 function mousedownRight(event) {
   if(mousedownID==-1)mousedownID = setInterval(whilemousedownRight, 2);
 }
@@ -36,15 +39,14 @@ function whilemousedownRight() {
    var left = parseFloat(window.getComputedStyle(user).getPropertyValue("left"));
     if(left < 300){
         var newleft = left + 3;
-        var px = "px";
         var newnewleft = newleft + px;
         user.style.left = newnewleft;
     }
 }
-//document.getElementById("right").addEventListener("mousedown", mousedownRight);
-//document.getElementById("right").addEventListener("mouseup", mouseupRight);
-document.getElementById("right").addEventListener("onpointerdown", mousedownRight);
-document.getElementById("right").addEventListener("onpointerup", mouseupRight);
+document.getElementById("right").addEventListener("mousedown", mousedownRight);
+document.getElementById("right").addEventListener("mouseup", mouseupRight);
+document.getElementById("right").addEventListener("ontouchstart", mousedownRight);
+document.getElementById("right").addEventListener("ontouchend", mouseupRight);
 
 document.addEventListener("keydown", event => {
   if (event.isComposing || event.keyCode === 229) {
@@ -63,3 +65,26 @@ document.addEventListener("keyup", event => {
   if(event.key==="ArrowRight"||event.key==="d"){mouseupRight();}
   
 });
+
+var counter = 0;
+var checkHit = setInterval(function() {
+    var top = parseFloat(window.getComputedStyle(ball).getPropertyValue("top"));
+    var ballleft = parseFloat(window.getComputedStyle(balldiv).getPropertyValue("left"));
+    var userleft = parseFloat(window.getComputedStyle(user).getPropertyValue("left"));
+    if(userleft-10<ballleft&&userleft+110>ballleft){
+        var shawn = true;
+        counter++;
+    }else{
+        alert("you missed, score: " + counter);
+        ball.classList.remove("animation");
+        var left = ballleft + px;
+        ball.style.left = left;
+        balldiv.style.animation = "none";
+        clearInterval(checkHit);
+        restart.style.display = "block";
+    }
+//    ball.style.animation = "slideY 5s infinite linear";
+//    balldiv.style.animation = "slideX 1.1s infinite linear";
+//    var newball = ballleft + px;
+//    ball.style.left = newball;
+}, 4999);
