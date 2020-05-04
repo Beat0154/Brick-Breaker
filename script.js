c9a2893cc2199152e168f6219e012c4208110557
Shawn Beaton <beat0154@algonquinlive.com>
2020-05-04 14:32:46 -0400

    slider

---
+++ script.js
@@ -0,0 +1,75 @@
+var mousedownID = -1; 
+function mousedownLeft(event) {
+  if(mousedownID==-1)mousedownID = setInterval(whilemousedownLeft, 2);
+}
+function mouseupLeft(event) {
+   if(mousedownID!=-1) {
+     clearInterval(mousedownID);
+     mousedownID=-1;
+   }
+}
+function whilemousedownLeft() {
+   var left = parseFloat(window.getComputedStyle(user).getPropertyValue("left"));
+    if(left > 0){
+        var newleft = left - 3;
+        var px = "px";
+        var newnewleft = newleft + px;
+        user.style.left = newnewleft;
+    }
+}
+document.getElementById("left").addEventListener("mousedown", mousedownLeft);
+document.getElementById("left").addEventListener("mouseup", mouseupLeft);
+
+
+
+
+var mousedownID = -1; 
+function mousedownRight(event) {
+  if(mousedownID==-1)mousedownID = setInterval(whilemousedownRight, 2);
+}
+function mouseupRight(event) {
+   if(mousedownID!=-1) {
+     clearInterval(mousedownID);
+     mousedownID=-1;
+   }
+}
+function whilemousedownRight() {
+   var left = parseFloat(window.getComputedStyle(user).getPropertyValue("left"));
+    if(left < 300){
+        var newleft = left + 3;
+        var px = "px";
+        var newnewleft = newleft + px;
+        user.style.left = newnewleft;
+    }
+}
+document.getElementById("right").addEventListener("mousedown", mousedownRight);
+document.getElementById("right").addEventListener("mouseup", mouseupRight);
+
+//function keyDown(event){
+//    if(event.keyCode == '37'){
+//        mousedownLeft();
+//    }
+//    if(event.keyCode == '39'){
+//        mousedownRight();
+//    }
+//}
+//document.addEventListener("keydown", keydown);
+
+
+document.addEventListener("keydown", event => {
+  if (event.isComposing || event.keyCode === 229) {
+    return;
+  }
+  if(event.key==="ArrowLeft"||event.key==="a"){mousedownLeft();}
+  if(event.key==="ArrowRight"||event.key==="d"){mousedownRight();}
+  
+});
+
+document.addEventListener("keyup", event => {
+  if (event.isComposing || event.keyCode === 229) {
+    return;
+  }
+  if(event.key==="ArrowLeft"||event.key==="a"){mouseupLeft();}
+  if(event.key==="ArrowRight"||event.key==="d"){mouseupRight();}
+  
+});
